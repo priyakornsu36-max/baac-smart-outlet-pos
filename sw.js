db20138c26b54b674c08920ac502689d9f163799
priyakornsu36-max <priyakorn.su36@gmail.com>
2026-09-17 20:56:44 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'baac-pos-v1';
+
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => {
+        return cached || fetch(event.request);
+      })
+  );
+});
